bfe8c93507a28dbb1d3b0cfb5ac7c77745f884db
Chenjie <chenjieh@ucm.es>
2022-04-26 00:18:53 +0200

    Moviemiento de la clase Ball y correciones(sin probar aun)

--- field.py
+++ field.py
@@ -2,26 +2,28 @@ from multiprocessing.connection import Listener
 from multiprocessing import Process, Manager, Value, Lock
 import traceback
 import sys
+import math
 
 GOALKEEPER = 0
 SHOOTER = 1
 SIDESSTR = ["goalkeeper", "shooter"]
-SIZE = (700, 525)
+SIZE = (700, 700)  #la imagen es de 700x700
+GAMMA = 2*math.pi/30 #variacion del angulo
 DELTA = 10
 
 class Player():
     def __init__(self):
         self.posx = SIZE[0]/2
-
+    #lo paso a la clase hijo, voy a hacer uno diferente para shooter
+    """ 
     def move(self, dir):
-        print(self.posx)
         sign = -1 if dir == "left" else 1
         self.posx = self.posx + sign*DELTA
         if self.posx < 0:
             self.posx = 0
         elif self.posx > SIZE[0]:
             self.posx = SIZE[0]
-        print(self.posx)
+    """
     
     def get_pos(self):
         return [self.posx, self.posy]
@@ -29,15 +31,56 @@ class Player():
 class Goalkeeper(Player):
     def __init__(self):
         super().__init__()
-        self.posy = SIZE[1]/2 - 80
+        #he cambiado la posicion
+        self.posy = SIZE[1]/2 + 300
+    
+    def move(self, dir):
+        sign = -1 if dir == "left" else 1
+        self.posx = self.posx + sign*DELTA
+        if self.posx < 0:
+            self.posx = 0
+        elif self.posx > SIZE[0]:
+            self.posx = SIZE[0]
         
-class Shooter(Player):
-    def __init__(self):
+class Shooter(Player): #es el propio balon en si
+    #NOTA: no se si hacer que rebote o no, pues es un poco irrelevante
+    #lo dejare para despues
+    def __init__(self, speed):
         super().__init__()
-        self.posy = SIZE[1]/2 + 180
+        self.posy = SIZE[1]/2 - 300
+        self.speed = speed #modulo de la velocidad
+        self.angle = 0.0 #angulo del disparo NOTA: tipo float, pero creo que asi escrito esta muy feo
+        self.velocity = [int(self.speed * math.cos(self.angle)), int(self.speed * math.sin(self.angle))]
+    
+    def get_angle(self):
+        return self.angle
         
+    def move(self, dir): #nombre anterior: adjust_angle, se pasa a llamar move, por reutilizar la funcion en game
+        sign = -1 if dir == "left" else 1
+        self.angle = self.angle + sign*GAMMA
+        if self.angle < -math.pi:
+            self.angle = -math.pi
+        elif self.angle > math.pi:
+            self.angle = math.pi
+    
+        #actualiza la velocidad(vector) lo hago asi para evitar calcular tantos cosenos y senos
+    def update_velocity(self):
+        self.velocity = [int(self.speed * math.cos(self.angle)), int(self.speed * math.sin(self.angle))]
+    
+    def update(self): #actualiza la posicion de la pelota
+        self.posx += self.velocity[0]
+        self.posy += self.velocity[1]
+
+    def bounce(self, AXIS):
+        self.velocity[AXIS] = -self.velocity[AXIS]
+    
+    def __str__(self):
+        pass
+
+#incluida en clase Shooter
+"""
 class Ball():
-	def __init__(self, speed):
+    def __init__(self, speed):
 		self.speed = speed
 		self.posx = SIZE[0]/2
 		self.posy = SIZE[1]/2 + 180
@@ -47,19 +90,29 @@ class Ball():
 	
 	def shoot(self):
 		self.posx += 3*self.speed
-		
+"""
+
+
 class Game():
     def __init__(self, manager):
-        self.players = manager.list([Goalkeeper(), Shooter()])
+        self.players = manager.list([Goalkeeper(), Shooter(speed=2)])
+        self.score = manager.list([0, 0])
         self.running = Value('i', 1)
-        self.ball = manager.list( [Ball(2)] )
+        self.ball_moving = Value('i', 0)
+        #self.ball = manager.list( [Ball(2)] )
         self.lock = Lock()
         
     def get_player(self, type):
         return self.players[type]
+    
+    def get_score(self):
+        return list(self.score)
         
     def is_running(self):
         return self.running.value == 1
+    
+    def is_ball_moving(self):
+        return self.ball_moving.value == 1
 
     def move(self, type, dir):
         self.lock.acquire()
@@ -70,13 +123,31 @@ class Game():
         
     def stop(self):
         self.running.value = 0
+    
+    def shoot_ball(self):
+        self.lock.acquire()
+        self.ball_moving.value = 1
+        p = self.players[SHOOTER]
+        p.update_velocity()
+        self.players[SHOOTER] = p
+        self.lock.release()
+    
+    def move_ball(self):
+        self.lock.acquire()
+        p = self.players[SHOOTER]
+        p.update()
+        #controlar la puntuacion aqui con condicionales
+        self.players[SHOOTER] = p
+        self.lock.release()
         
     def get_info(self):
         info = {
-            'pos_goalkeeper': self.players[0].get_pos(),
-            'pos_shooter': self.players[1].get_pos(),
+            'pos_goalkeeper': self.players[GOALKEEPER].get_pos(),
+            'pos_shooter': self.players[SHOOTER].get_pos(),
+            'ball_angle': self.players[SHOOTER].get_angle(),
             'score': [0, 0],
-            'is_running': self.is_running()
+            'is_running': self.is_running(),
+            'ball_moving': self.is_ball_moving()
         }
         return info
 
@@ -93,8 +164,12 @@ def player(type, conn, game):
                 command = conn.recv()
                 if command == "left" or command == "right":
                     game.move(type, command)
+                elif command == "shoot": #comando nuevo
+                    game.shoot_ball()
                 elif command == "quit":
                     game.stop()
+            if game.is_ball_moving(): #mueve la pelota si se ha disparado
+                game.move_ball()
             conn.send(game.get_info())
 
     except:
--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from multiprocessing.connection import Client
 import traceback
 import pygame
 import sys, os
+import math
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -11,196 +12,215 @@ YELLOW = (255,255,0)
 GREEN = (0,255,0)
 X = 0
 Y = 1
-SIZE = (700, 525)
+SIZE = (700, 700) #la imagen es de 700x700
 
 FPS = 60
 
 PLAYER_COLOR = [BLUE, YELLOW]
-PLAYER_HEIGHT = 25
-PLAYER_WIDTH = 25
+PLAYER_HEIGHT = 30
+PLAYER_WIDTH = 30
+
+BALL_SIZE = 20
+BALL_COLOR = YELLOW
 
 GOALKEEPER = 0
 SHOOTER = 1
 TYPE = ['goalkeeper', 'shooter']
 
 class Player():
-	def __init__(self, type):
-		self.pos = [None, None]
-		self.type = type
-    
-	def get_pos(self):
-		return self.pos
-    
-	def get_type(self):
-		return self.type
-    
-	def set_pos(self, pos):
-		self.pos = pos
-    
-	def __str__(self):
-		pass
-    
-class Ball(): # solo se mueve en linea recta
-    def __init__(self):
-        self.speed = None
+    def __init__(self, type):
         self.pos = [None, None]
-        
-	def set_pos(self,pos):
-		self.pos = pos
+        self.type = type
+    
+    def get_pos(self):
+        return self.pos
 
-	def get_por(self):
-		return self.pos
+    def get_type(self):
+        return self.type
 
-	def __str__(self):
-		pass
+    def set_pos(self, pos):
+        self.pos = pos
+
+    def __str__(self):
+        pass
+
+#lo voy a hacer como subclase de Player
+class Ball(Player): # solo se mueve en linea recta
+    def __init__(self):
+        super().__init__(SHOOTER)
+        #self.speed = None
+        self.angle = None
+        #self.pos = [None, None] ya lo hereda de Player(class)
+
+    def set_angle(self, angle):
+        self.angle = angle
+
+    def get_angle(self):
+        return self.angle
+
+    def __str__(self):
+        pass
 
 class Game():
-	def __init__(self):
-		self.players = [Player(k) for k in range(2)]
-		self.ball = Ball()
-		self.score = [0, 0]
-		self.running = True
+    def __init__(self):
+        self.player = Player(GOALKEEPER)
+        self.ball = Ball()
+        self.score = [0, 0]
+        self.running = True
+        self.ball_moving = False
 
-	def get_player(self, type):
-		return self.players[type]
+    def get_player(self, type):
+        return self.player
+    
+    def get_ball(self):
+        return self.ball
 
-	def set_pos_player(self, type, pos):
-		self.players[type].set_pos(pos)
+    def set_pos_player(self, pos):
+        self.player.set_pos(pos)
+    
+    def set_pos_ball(self, pos):
+        self.ball.set_pos(pos)
+    
+    def set_angle_ball(self, angle):
+        self.ball.set_angle(angle)
 
-	def get_score(self):
-		return self.score
-       
-	def get_ball(self):
-		return self.ball
+    def get_score(self):
+        return self.score
 
-	def set_pos_ball(self, pos):
-		self.ball.set_pos(pos)
+    def set_score(self, score):
+        self.score = score
+
+    def update(self, gameinfo):
+        self.set_pos_player(gameinfo['pos_goalkeeper'])
         
-	def set_score(self, score):
-		self.score = score
-
-	def update(self, gameinfo):
-		self.set_pos_player(GOALKEEPER, gameinfo['pos_goalkeeper'])
-		self.set_pos_player(SHOOTER, gameinfo['pos_shooter'])
-		self.set_pos_ball(gameinfo['pos_ball'])
-		self.set_score(gameinfo['score'])
-		self.running = gameinfo['is_running']
-
-	def stop(self):
-		self.running = False
-
-	def __str__(self):
-		pass
-
-class Cube(pygame.sprite.Sprite):
-	def __init__(self, player):
-		super().__init__()
-		self.image = pygame.Surface([PLAYER_WIDTH, PLAYER_HEIGHT])
-		self.image.fill(BLACK)
-		self.image.set_colorkey(BLACK)
-		self.player = player
-		color = PLAYER_COLOR[self.player.get_type()]
-		pygame.draw.rect(self.image, color, [0,0,PLAYER_WIDTH, PLAYER_HEIGHT])
-		self.rect = self.image.get_rect()
-		self.update()
-
-	def update(self):
-		pos = self.player.get_pos()
-		self.rect.centerx, self.rect.centery = pos
-
-	def __str__(self):
-		return f"S<{self.player}>"
+        self.set_pos_ball(gameinfo['pos_shooter'])
         
+        self.set_angle_ball(gameinfo['ball_angle'])
+        self.set_score(gameinfo['score'])
+        self.running = gameinfo['is_running']
+        self.ball_moving = gameinfo['ball_moving']
+
+    def stop(self):
+        self.running = False
+
+    def __str__(self):
+        pass
+
+class Square(pygame.sprite.Sprite):
+    def __init__(self, player):
+        super().__init__()
+        self.image = pygame.Surface([PLAYER_WIDTH, PLAYER_HEIGHT])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        self.player = player
+        color = PLAYER_COLOR[self.player.get_type()]
+        pygame.draw.rect(self.image, color, [0,0,PLAYER_WIDTH, PLAYER_HEIGHT])
+        self.rect = self.image.get_rect()
+        self.update()
+
+    def update(self):
+        pos = self.player.get_pos()
+        self.rect.centerx, self.rect.centery = pos
+
+    def __str__(self):
+        return f"S<{self.player}>"
+
 class Circle(pygame.sprite.Sprite):
-	def __init__(self, ball):
-		super().__init__()
-		self.ball = ball
-		self.image = pygame.Surface((BALL_SIZE, BALL_SIZE))
-		self.image.fill(BLACK)
-		self.image.set_colorkey(BLACK)
-		pygame.draw.rect(self.image, BALL_COLOR, [0, 0, BALL_SIZE, BALL_SIZE])
-		self.rect = self.image.get_rect()
-		self.update()
+    def __init__(self, ball):
+        super().__init__()
+        self.image = pygame.Surface([BALL_SIZE, BALL_SIZE])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        self.ball = ball
+        pygame.draw.circle(self.image, BALL_COLOR, [0,0], BALL_SIZE//2)
+        self.rect = self.image.get_rect()
+        self.update()
 
-	def update(self):
-		pos = self.ball.get_pos()
-		self.rect.centerx, self.rect.centery = pos
+    def update(self):
+        pos = self.ball.get_pos()
+        #print(pos)
+        self.rect.centerx, self.rect.centery = pos
+    
+    def __str__(self):
+        return f"S<{self.ball}>"
 
 class Display():
-	def __init__(self, game):
-		self.game = game
-		self.cube = [Cube(self.game.get_player(k)) for k in range(2)]
-		self.cube_sprite = pygame.sprite.Group()
-		for cube in self.cube:
-			self.cube_sprite.add(cube)
-		self.ball = Circle(self.game.get_ball)
-		self.screen = pygame.display.set_mode(SIZE)
-		self.clock = pygame.time.Clock()
-		self.background = pygame.image.load('fondo.jpeg')
-		pygame.init()
+    def __init__(self, game):
+        self.game = game
+        self.square = Square(self.game.get_player(GOALKEEPER))
+        self.circle = Circle(self.game.ball)
+        self.all_sprite = pygame.sprite.Group()
+        
+        self.all_sprite.add(self.square)
+        self.all_sprite.add(self.circle)
+        
+        self.screen = pygame.display.set_mode(SIZE)
+        self.clock = pygame.time.Clock()
+        self.background = pygame.image.load('fondo.jpeg')
+        pygame.init()
 
-	def analyze_events(self):
-		events = []
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					events.append("quit")
-				elif event.key == pygame.K_LEFT:
-					events.append("left")
-				elif event.key == pygame.K_RIGHT:
-					events.append("right")
-				elif event.key == pygame.K_SPACE:
-					events.append("shoot")
-			elif event.type == pygame.QUIT:
-				events.append("quit")
-		return events
-    
-	def refresh(self):
-		self.cube_sprite.update()
-		self.screen.blit(self.background, (0, 0))
-		score = self.game.get_score()
-		font = pygame.font.Font(None, 74)
-		text = font.render(f"{score[GOALKEEPER]}", 1, WHITE)
-		self.screen.blit(text, (250, 10))
-		text = font.render(f"{score[SHOOTER]}", 1, WHITE)
-		self.screen.blit(text, (SIZE[X]-250, 10))
-		self.cube_sprite.draw(self.screen)
-		pygame.display.flip()
+    def analyze_events(self, type):
+        events = []
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    events.append("quit")
+                elif event.key == pygame.K_LEFT:
+                    events.append("left")
+                elif event.key == pygame.K_RIGHT:
+                    events.append("right")
+                elif event.key == pygame.K_SPACE and type == SHOOTER and not self.game.ball_moving:
+                    events.append("shoot")
+            elif event.type == pygame.QUIT:
+                events.append("quit")
+        return events
 
-	def tick(self):
-		self.clock.tick(FPS)
-    
-	@staticmethod
-	def quit():
-		pygame.quit()
+    def refresh(self):
+        self.all_sprite.update()
+        self.screen.blit(self.background, (0, 0))
+        score = self.game.get_score()
+        font = pygame.font.Font(None, 74)
+        text = font.render(f"{score[GOALKEEPER]}", 1, WHITE)
+        self.screen.blit(text, (250, 10))
+        text = font.render(f"{score[SHOOTER]}", 1, WHITE)
+        self.screen.blit(text, (SIZE[X]-250, 10))
+        self.all_sprite.draw(self.screen)
+        pygame.display.flip()
+
+    def tick(self):
+        self.clock.tick(FPS)
+
+    @staticmethod
+    def quit():
+        pygame.quit()
 
 def main(ip_address):
-	try:
-		with Client((ip_address, 11235), authkey=b'secret password') as conn:
-			game = Game()
-			type, gameinfo = conn.recv()
-			print(f"I am playing {TYPE[type]}")
-			game.update(gameinfo)
-			display = Display(game)
-			while game.running:
-				events = display.analyze_events()
-				for ev in events:
-					conn.send(ev)
-					if ev == 'quit':
-						game.stop()
-					conn.send("next")
-				gameinfo = conn.recv()
-				game.update(gameinfo)
-				display.refresh()
-				display.tick()
-	except:
-		traceback.print_exc()
-	finally:
- 		pygame.quit()
+    try:
+        with Client((ip_address, 11235), authkey=b'secret password') as conn:
+            game = Game()
+            type, gameinfo = conn.recv()
+            print(f"I am playing {TYPE[type]}")
+            game.update(gameinfo)
+            display = Display(game)
+            while game.running:
+                events = display.analyze_events(type)
+                for ev in events:
+                    conn.send(ev)
+                    if ev == 'quit':
+                        game.stop()
+                    conn.send("next")
+                gameinfo = conn.recv()
+                game.update(gameinfo)
+                display.refresh()
+                display.tick()
+    except:
+        traceback.print_exc()
+    finally:
+        pygame.quit()
 
 
 if __name__=="__main__":
-	ip_address = "127.0.0.1"
-	if len(sys.argv)>1:
-		ip_address = sys.argv[1]
-	main(ip_address)
+    ip_address = "127.0.0.1"
+    if len(sys.argv)>1:
+        ip_address = sys.argv[1]
+    main(ip_address)
